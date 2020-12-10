ebb1dd04173ea803e1477f3f4556f8631475ce8e
github username <48257792+ry-thornton@users.noreply.github.com>
2020-12-10 16:44:50 -0500

    integrated mapbox and making requests to api

---
+++ coffeehood-app/components/Display.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+
+function Display(props){
+
+    const [location, setLocation] = useState("")
+
+    useEffect(()=> {
+
+    })
+
+    let changeHandler = (event) => {
+        console.log(location)
+        setLocation(event.target.value)
+    }
+
+    let submitHandler = (event) => {
+        event.preventDefault()
+        props.search(location)
+    }
+
+       return (
+        <div>
+            <nav className="navbar is-transparent">
+                <div className="navbar-item">
+                    <span>CoffeeHood</span>
+                </div>
+                <div className="navbar-end">
+                    <div className="buttons">
+                    <button className="button ">Get Matched!</button>
+                    <button className="button">Login</button>
+                    </div>
+                    
+                </div>
+            </nav>
+            <form onSubmit={submitHandler}>
+                <input className="input" placeholder="Enter an address" type="text" value={location} onChange={changeHandler}/>
+                <input className="button" type="submit" value="Search"/>
+            </form>
+         
+        </div>
+       )
+    
+    
+    
+}
+
+export default Display
---
+++ coffeehood-app/pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Display from '../components/Display.js';
+import styles from '../styles/Home.module.css';
+import {useState, useEffect} from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+
+export default function Home() {
+
+  const [lat, setLat] = useState(40.7228);
+  const [long, setLong] = useState(-74.0060);
+  const [zoom, setZoom] = useState(12);
+
+  
+  useEffect(()=>{
+    mapboxgl.accessToken = 'pk.eyJ1IjoicnktdGhvcm50b24iLCJhIjoiY2szN2hrMWU2MDNrajNjbnVlYzA1cGVoeSJ9.3quDBtJRLxfhmm_QXK8OHQ';
+    const map = new mapboxgl.Map({
+      container: 'mapContainer',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [long, lat],
+      zoom: zoom
+      });
+  });
+
+
+  let search = (location) => {
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw&cachebuster=1607636381091&autocomplete=true&country=us&types=poi&proximity=-74.0060%2C%2040.7128&limit=7`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  }
+
+  return (
+    <div>
+
+      <Head>
+        <title>
+          CoffeeHood App
+        </title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"/>
+        <link href='https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css' rel='stylesheet' />
+      </Head>
+        <div className="content container">
+          <Display search={search}/>
+          <br/>
+          <div id="mapContainer"/>
+
+        </div>
+      </div>
+  )
+}
